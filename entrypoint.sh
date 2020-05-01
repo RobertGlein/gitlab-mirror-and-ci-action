3a271bef8ca387d9ff67a6b7f3a55cd2aa806c09
Robért Glein <38926379+RobertGlein@users.noreply.github.com>
2020-05-01 12:58:16 -0600

    set mirror first

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,7 @@ branch=$(git symbolic-ref --short HEAD)
 sh -c "git config --global credential.username $GITLAB_USERNAME"
 sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
+sh -c "git remote add mirror $*"
 if [[ -n "${REMOVE_BRANCH}" ]]; then
    sh -c "echo ${REMOVE_BRANCH}"
    if [[ "${REMOVE_BRANCH}"="true" ]]; then
@@ -24,7 +25,6 @@ if [[ -n "${REMOVE_BRANCH}" ]]; then
       sh -c "git remote remove $branch"
    fi
 fi
-sh -c "git remote add mirror $*"
 sh -c "echo pushing to $branch branch at $(git remote get-url --push mirror)"
 sh -c "git push mirror $branch"
 
